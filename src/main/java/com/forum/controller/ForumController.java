2f1a077dba3bb2845005498869ac9616e1bd2c68
Sophia <>
2024-07-14 00:17:01 +0800

    上傳論壇前台功能

---
+++ src/main/java/com/forum/controller/ForumController.java
@@ -0,0 +1,212 @@
+package com.forum.controller;
+
+import java.io.IOException;
+import java.sql.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.forum.model.ForumPostVO;
+import com.forum.model.ForumReplyVO;
+import com.forum.model.ForumService;
+import com.morning.mem.model.MemVO;
+
+@Controller
+@RequestMapping("forum")
+public class ForumController {
+	
+	@Autowired
+	ForumService forumSvc;
+
+	/***** 畫面設定 *****/
+
+	@GetMapping("")
+    public String listAllPost(
+    	@RequestParam(value = "postId", required = false) String postId,
+    	ModelMap model) {
+		if ( postId != null ) {
+			ForumPostVO forumPostVO = forumSvc.getOnePost(Integer.valueOf(postId));
+			model.addAttribute("forumPostVO", forumPostVO);
+			
+			ForumReplyVO forumReplyVO = new ForumReplyVO();
+			forumReplyVO.setForumPostVO(new ForumPostVO());
+			forumReplyVO.getForumPostVO().setPostId(Integer.valueOf(postId));
+			
+			
+			model.addAttribute("forumReplyVO", forumReplyVO);
+			
+			return "front-end/forum/detail";
+		}
+		return "front-end/forum/listAllPost";
+    }
+	
+	@GetMapping("addPost")
+	public String addPost(ModelMap model) {
+		ForumPostVO forumPostVO = new ForumPostVO();
+		model.addAttribute("forumPostVO", forumPostVO);
+		return "front-end/forum/addPost";
+	}
+	
+	@PostMapping("editPost")
+	public String editPost(
+			@RequestParam("postId") String postId,
+			ModelMap model) {
+		
+		ForumPostVO forumPostVO = forumSvc.getOnePost(Integer.valueOf(postId));
+		model.addAttribute("forumPostVO", forumPostVO);
+		
+		return "front-end/forum/editPost";
+	}
+	
+	/***** 畫面請求操作 *****/
+	
+	@PostMapping("insert")
+	public String insert(@Valid ForumPostVO forumPostVO, 
+			BindingResult result, ModelMap model,
+			RedirectAttributes redirectAttributes) throws IOException {
+		
+		// 去除BindingResult中upFiles欄位的FieldError紀錄
+		result = removeFieldError(forumPostVO, result, "postImg");
+		result = removeFieldError(forumPostVO, result, "postTime");
+		
+		// 儲存現在的日期
+		Date sqlDate = new Date(System.currentTimeMillis());
+        forumPostVO.setPostTime(sqlDate);
+        
+        System.out.println(forumPostVO);
+		
+		// 如果有錯誤，回到上一頁
+		if (result.hasErrors()) {
+			return "front-end/forum/addPost";
+		}
+		
+		forumSvc.addPost(forumPostVO);
+		
+		List<ForumPostVO> list = forumSvc.getAll();
+		model.addAttribute("forumPostListData", list);
+		
+		redirectAttributes.addFlashAttribute("success", "- (新增成功)");
+		
+		// 新增成功後重導至文章列表
+		return "redirect:/forum/";
+	}
+	
+	@PostMapping("update")
+	public String update(@Valid ForumPostVO forumPostVO, 
+			BindingResult result, ModelMap model,
+			RedirectAttributes redirectAttributes) throws IOException {
+		
+		// 去除BindingResult中upFiles欄位的FieldError紀錄
+		result = removeFieldError(forumPostVO, result, "postImg");
+		result = removeFieldError(forumPostVO, result, "postTime");
+		
+		// 如果有輸入內容有錯誤，回到上一頁
+		if (result.hasErrors()) {
+			return "front-end/forum/editPost";
+		}
+		
+		// 更新文章資料
+		forumSvc.updatePost(forumPostVO);
+
+		forumPostVO = forumSvc.getOnePost(Integer.valueOf(forumPostVO.getPostId()));
+		
+		model.addAttribute("forumPostVO", forumPostVO);
+		
+		redirectAttributes.addFlashAttribute("success", "- (修改成功)");
+		return "redirect:/forum?postId=" + forumPostVO.getPostId();
+	}
+
+	@PostMapping("delete")
+	public String delete(@RequestParam("postId") String postId,
+			ModelMap model, RedirectAttributes redirectAttributes) {
+		forumSvc.deletePost(Integer.valueOf(postId));
+		
+		model.addAttribute("forumPostListData", forumSvc.getAll());
+		redirectAttributes.addFlashAttribute("success", "- (刪除成功)");
+		
+		return "redirect:/forum/";  // 導回文章列表
+	}
+	
+    @PostMapping("addReply")
+    public ResponseEntity<?> addReply(
+		 @RequestParam("postId") String postIdStr,
+		 @RequestParam("replyContent") String replyContent,
+		 HttpSession session) {
+    	
+    	MemVO memVO = (MemVO) session.getAttribute("memVO");
+    	if ( memVO == null ) {
+    		return ResponseEntity.status(HttpStatus.OK).body(null);
+    	}
+		
+		Integer postId = Integer.valueOf(postIdStr);
+		
+		// 回覆VO 設定文章編號
+    	ForumReplyVO forumReplyVO = new ForumReplyVO();
+    	forumReplyVO.setForumPostVO(new ForumPostVO());
+    	forumReplyVO.getForumPostVO().setPostId(postId);
+    	
+    	forumReplyVO.setMemVO(memVO);
+    	forumReplyVO.setReplyContent(replyContent);
+    	
+    	Date sqlDate = new Date(System.currentTimeMillis());
+    	forumReplyVO.setReplyTime(sqlDate);
+    	
+    	ForumReplyVO savedForumReplyVO = forumSvc.addReply(forumReplyVO);
+    	
+//    	System.out.println("savedForumReplyVO = " + savedForumReplyVO.getForumPostVO().getMemVO().getMemName());
+    	if ( savedForumReplyVO != null ) {
+    		return ResponseEntity.status(HttpStatus.OK).body(savedForumReplyVO);
+    	}
+		
+    	return ResponseEntity.status(HttpStatus.OK).body("fail");
+    }
+	
+	@PostMapping("deleteReply")
+    public ResponseEntity<?> deleteReply(@RequestParam("replyId") String replyIdStr) {
+		
+		Integer replyId = Integer.valueOf(replyIdStr);
+		if ( forumSvc.deleteReply(replyId) ) {
+			return ResponseEntity.status(HttpStatus.OK).body("success");
+		} else {
+			return ResponseEntity.status(HttpStatus.OK).body("fail");
+		}
+    }
+	
+	/***** 資料取得 *****/
+    @ModelAttribute("forumPostListData")
+	protected List<ForumPostVO> getForumPostListData(Model model) {
+		
+		return forumSvc.getAll();
+	}
+	
+    
+	// 去除BindingResult中某個欄位的FieldError紀錄
+	public BindingResult removeFieldError(ForumPostVO postVO, BindingResult result, String removedFieldname) {
+		List<FieldError> errorsListToKeep = result.getFieldErrors().stream()
+				.filter(fieldname -> !fieldname.getField().equals(removedFieldname))
+				.collect(Collectors.toList());
+		result = new BeanPropertyBindingResult(postVO, "forumPostVO");
+		for (FieldError fieldError : errorsListToKeep) {
+			result.addError(fieldError);
+		}
+		return result;
+	}
+}
---
+++ src/main/java/com/forum/model/ForumReplyVO.java
@@ -0,0 +1,86 @@
+package com.forum.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.morning.mem.model.MemVO;
+
+@Entity
+@Table(name = "forum_reply")
+public class ForumReplyVO implements java.io.Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer replyId;
+	private String replyContent;
+	private Date replyTime;
+	
+	private MemVO memVO;
+	private ForumPostVO forumPostVO;
+	
+
+	@Id
+	@Column(name = "reply_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getReplyId() {
+		return replyId;
+	}
+	public void setReplyId(Integer replyId) {
+		this.replyId = replyId;
+	}
+
+
+	@Column(name = "reply_content")
+	public String getReplyContent() {
+		return replyContent;
+	}
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+
+
+	@Column(name = "reply_time")
+	public Date getReplyTime() {
+		return replyTime;
+	}
+	public void setReplyTime(Date replyTime) {
+		this.replyTime = replyTime;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "post_id", referencedColumnName = "post_id")
+	public ForumPostVO getForumPostVO() {
+		return forumPostVO;
+	}
+
+	public void setForumPostVO(ForumPostVO forumPostVO) {
+		this.forumPostVO = forumPostVO;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name="mem_no")
+	public MemVO getMemVO() {
+		return memVO;
+	}
+	public void setMemVO(MemVO memVO) {
+		this.memVO = memVO;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "ForumReplyVO{" +
+                "replyId=" + replyId +
+                ", replyContent='" + replyContent + '\'' +
+                ", replyTime=" + replyTime +
+                ", memName=" + (forumPostVO.getMemVO() != null ? forumPostVO.getMemVO().getMemName() : null) +
+                '}';
+	}
+}
