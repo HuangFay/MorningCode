74e55012eb04be595e159e2d2c6a00ed3a739c63
PIN YAN <angelspeak0505@gmail.com>
2024-07-18 14:22:48 +0800

    菜單頁面css，加入取消收藏

--- src/main/java/com/morning/collect/model/CollectRepository.java
+++ src/main/java/com/morning/collect/model/CollectRepository.java
@@ -7,16 +7,25 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CollectRepository extends JpaRepository<CollectVO, Integer> {
 
-	//照編號刪除
+	// 取消收藏
 	@Transactional
 	@Modifying
-	@Query(value = "delete from collect where id=?1", nativeQuery = true)
-	void deleteById(Integer id);
-	
-	//查詢會員收藏
+	@Query(value = "delete from collect where mem_no=?1 and meals_id=?2", nativeQuery = true)
+	void deleteByMemNoAndMealsId(Integer memNo, Integer mealsId);
+
+	// 查詢會員收藏
 	@Query(value = "select * from collect where mem_no = ?1", nativeQuery = true)
 	List<CollectVO> findByMemNo(Integer memNo);
+
+	// 查詢會員收藏
+	@Query(value = "select count(*) > 0 from collect where mem_no = ?1 and meals_id = ?2", nativeQuery = true)
+	boolean existsByMemNoAndMealsId(Integer memNo, Integer mealsId);
+
+	// 用輸入的memNo去CollectVO查mealsId
+	@Query("SELECT c.mealsVO.mealsId FROM CollectVO c WHERE c.memVO.memNo = :memNo")
+	List<Integer> findMealsIdsByMemNo(@Param("memNo") Integer memNo);
 }
