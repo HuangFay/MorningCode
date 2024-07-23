31c322c92feae28e6daac426ef192161f76f65a2
Sophia <>
2024-07-24 07:17:40 +0800

    修正 討論區 留言API回傳缺少回傳id

--- src/main/java/com/morning/forum/model/ForumReplyDTO.java
+++ src/main/java/com/morning/forum/model/ForumReplyDTO.java
@@ -4,17 +4,27 @@ import java.text.SimpleDateFormat;
 
 public class ForumReplyDTO {
 	
+	private Integer replyId;
 	private String memName;
 	private String replyContent;
 	private String replyTime;
 	
 	public ForumReplyDTO(ForumReplyVO replyVO) {
+		this.replyId = replyVO.getReplyId();
 		this.memName = replyVO.getMemVO().getMemName();
 		this.replyContent = replyVO.getReplyContent();
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		String formattedDate = sdf.format(replyVO.getReplyTime());
 		this.replyTime = formattedDate;
+	}
+	
+	public Integer getReplyId() {
+		return replyId;
+	}
+
+	public void setReplyId(Integer replyId) {
+		this.replyId = replyId;
 	}
 	
 	public String getReplyTime() {
